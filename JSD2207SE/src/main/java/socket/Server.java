d87e1d67ccc6b5242148869a85905d4196277fb8
libreally <59366478+libreally@users.noreply.github.com>
2022-08-31 20:03:32 +0800

    Update Server.java

--- JSD2207SE/src/main/java/socket/Server.java
+++ JSD2207SE/src/main/java/socket/Server.java
@@ -16,6 +16,18 @@ public class Server {
     public Server(){
         try {
             System.out.println("正在启动服务端。。。");
+             /*
+                创建ServerSocket的同时要申请服务端口，该端口不能与系统其他程序
+                开启的端口一致，否则会抛出异常:
+                java.io.BindException:address already in use
+                解决办法:
+                更换端口
+                或
+                杀死占用该端口的程序进程:实际开发中8088很少被占用，通常都是由于我们
+                启动了两次服务端导致的(第一次启动已经占用了8088，那么第二次启动时
+                端口会显示被占用)
+
+             */
             serverSocket=new ServerSocket(8088);
             System.out.println("服务端启动完毕。。。");
         } catch (IOException e) {
@@ -26,8 +38,15 @@ public class Server {
         try {
             while(true){
                 System.out.println("等待客户端连接。。。");
+                /*
+                ServerSocket提供的一个重要方法:
+                Socket accept()
+                该方法是一个阻塞方法，调用会程序会卡住，直到一个客户端与服务端建立连接
+                为止，此时返回的Socket就可与连接的客户端进行交互了。
+             */
                 Socket socket = serverSocket.accept();//accept()接收客户端请求
                 System.out.println("一个客户端连接了...");
+                //启动一个线程处理与该客户端的交互
                 ClientHandler handler=new ClientHandler(socket);
                 Thread t=new Thread(handler);
                 t.start();
@@ -43,10 +62,12 @@ public class Server {
         Server server=new Server();
         server.start();
     }
-
+    /**
+     * 将处理客户端交互的操作在单独的线程上进行，这里的主要工作就是与某个客户端交互。
+     */
     private class ClientHandler implements Runnable{
         private Socket socket;
-        private  String host;//记录客户端
+        private  String host;//记录客户端ip
         public ClientHandler(Socket socket) {
             this.socket=socket;
             host = socket.getInetAddress().getHostAddress();
@@ -55,10 +76,19 @@ public class Server {
         @Override
         public void run() {
             try {
+                /*
+                    通过Socket获取输入流可以读取来自远端计算机发送过来的消息
+                 */
                 InputStream in = socket.getInputStream();//输入流
                 InputStreamReader isr = new InputStreamReader(in, StandardCharsets.UTF_8);//字节转换流 流链接
                 BufferedReader br = new BufferedReader(isr);//缓冲流
                 String message;//读取信息
+                /*
+                    这里循环读取客户端发送过来消息这里可能出现下面的异常:
+                    java.net.SocketException: Connection reset
+                    这个错误是由于远端异常断开导致的(没有进行TCP的挥手断开操作)，该异常无法通过
+                    逻辑避免。
+                 */
                 while ((message = br.readLine()) != null) {
                     System.out.println(host+"客户端：" + message);
                 }
