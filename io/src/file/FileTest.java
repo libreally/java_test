a3515ed46dd0c04228ce7d4713c4ab6bfd037bbf
libreally <2359304261@qq.com>
2022-09-22 22:40:25 +0800

    changes5

--- io/src/file/FileTest.java
+++ io/src/file/FileTest.java
@@ -3,6 +3,8 @@ package file;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  *
@@ -42,12 +44,95 @@ public class FileTest {
     public void test2(){
         File file1 = new File("hello.txt");
         File file2 = new File("D:\\abc\\java_test\\io\\he.txt");
-
         System.out.println(file1.getAbsoluteFile());
         System.out.println(file1.getName());
         System.out.println(file1.getPath());
         System.out.println(file1.getParent());
         System.out.println(file1.length());
         System.out.println(file1.lastModified());
+        System.out.println();
+        System.out.println(file2.getAbsoluteFile());
+        System.out.println(file2.getName());
+        System.out.println(file2.getPath());
+        System.out.println(file2.getParent());
+        System.out.println(file2.length());
+        System.out.println(new Date(file2.lastModified()) );
+        System.out.println();
     }
+    @Test
+    public void test3(){
+        File file = new File("D:\\java\\java_test\\io");
+        //获取指定目录下的所有文件或文件目录的名称数据
+        String[] list=file.list();
+        for (String s:list) {
+            System.out.println(s);
+        }
+        File[] files=file.listFiles();
+        for (File f:files) {
+            System.out.println(f);
+        }
+    }
+
+    /**
+     * boolean renameTo(File dest)
+     * 把文件重命名为指定文件名
+     */
+    @Test
+    public void test4(){
+        File file1 = new File("hello.txt");
+        File file2 = new File("D:\\abc\\java_test\\io\\hh.txt");
+
+        boolean renameTo = file1.renameTo(file2);
+        System.out.println(renameTo);
+
+    }
+
+    /**
+     * 判断功能
+     *         file.isFile(); 是否是文件
+     *         file.isDirectory();是否是目录
+     *         file.exists();是否存在
+     *         file.canWrite();是否可写
+     *         file.canRead();是否可读
+     *         file.isHidden();是否隐藏
+     */
+    @Test
+    public void test5(){
+        File file = new File("hello.txt");
+        boolean file1 = file.isFile();
+        System.out.println("file1 = " + file1);
+        boolean directory = file.isDirectory();
+        System.out.println("directory = " + directory);
+        boolean exists = file.exists();
+        System.out.println("exists = " + exists);
+        boolean write = file.canWrite();
+        System.out.println("write = " + write);
+        boolean read = file.canRead();
+        System.out.println("read = " + read);
+        boolean hidden = file.isHidden();
+        System.out.println("hidden = " + hidden);
+    }
+    /**
+     *  boolean createNewFile()   创建文件 若存在不创建
+     *  boolean mkdir()   创建文件目录，若存在不创建 如果此文件的上层不存在则不创建
+     *  boolean mkdirs()  创建文件目录，如果上层文件目录不存在，一并创建
+     *  boolean delete()   删除文件或文件夹
+     */
+    @Test
+    public void test6() throws IOException {
+        File file=new File("hi.txt");
+        if (file.exists()){
+            file.createNewFile();
+            System.out.println("success");
+        }else {
+            System.out.println("fail");
+        }
+        File file1=new File("D:\\abc\\java_test\\io\\io\\h2.txt");
+        //boolean mkdir = file1.mkdir();//此时上层目录不存在不能创建,应该使用mkdirs
+        boolean mkdirs = file1.mkdirs();
+        System.out.println("mkdirs="+mkdirs);
+
+    }
+
+
 }
